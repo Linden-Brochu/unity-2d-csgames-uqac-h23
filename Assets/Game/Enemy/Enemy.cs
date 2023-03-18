c77b4cdabf07a64499245bd4662d9ac48ba85b63
CS Games <participant@laboratoire.uqac.ca>
2023-03-18 11:59:54 -0400

    time to eat

--- Assets/Game/Enemy/Enemy.cs
+++ Assets/Game/Enemy/Enemy.cs
@@ -13,10 +13,20 @@ public class Enemy : MonoBehaviour
     [SerializeField] private GameObject GameObjectWithWaypoints;
 
     private float fieldOfViewRadius;
-
     [SerializeField] private GameObject fieldOfViewCircle;
 
+    Time StartSearchingTime; //Not working yet
+
     private int currentWaypointIndex = 0;
+    private bool searchFinished = false;
+    enum Status
+    {
+        Patrol,
+        Follow,
+        Search
+    }
+
+    private Status actionStatus = Status.Patrol;
 
     void Start()
     {
@@ -26,12 +36,9 @@ public class Enemy : MonoBehaviour
         //Start at first waypoint
         transform.position = waypoints[0].position;
 
+        //Get field of view radius from size in unity
         fieldOfViewRadius = fieldOfViewCircle.transform.localScale[0] / 2;
     }
-
-
-    //Patrouille
-
 
     void Move(Vector2 targetPos)
     {
@@ -40,7 +47,6 @@ public class Enemy : MonoBehaviour
 
     void followWaypoints()
     {
-        Debug.Log("Patrolling");
         Vector3 waypointPos = waypoints[currentWaypointIndex].position;
         if (Vector2.Distance(waypointPos, transform.position) < .1f)
         {
@@ -52,24 +58,82 @@ public class Enemy : MonoBehaviour
     }
 
 
+    void search()
+    {
+        float rotationSpeed = 1f;
+        transform.Rotate(new Vector3(0, 0, transform.rotation[2] + rotationSpeed));
+        //Debug.Log("rotation = " + transform.rotation[2].ToString());
+
+        if(true/*Time.time - StartSearchingTime > 5*/)
+        {
+            // 5 secondes
+            Debug.Log("Finished rotation");
+            transform.Rotate(new Vector3(0, 0, 0)); //Reset rotation
+            searchFinished = true;
+        }
+    }
+    
+
+
 
     // Update is called once per frame
     void Update()
     {
         //Niveau 1: suivi d'une trajectoire
         //followWaypoints();
+
         //Niveau 2: Detection joueur
+        //if (Vector2.Distance(transform.position, thePlayer.position) < fieldOfViewRadius)
+        //{
+        //    Debug.Log("Following player");
+        //    Move(thePlayer.position);
+        //}
+        //else
+        //{
+        //    followWaypoints();
+        //}
+
+        //Niveau 3
+
+
         if (Vector2.Distance(transform.position, thePlayer.position) < fieldOfViewRadius)
         {
-            Debug.Log("Following player");
-            Move(thePlayer.position);
+            actionStatus = Status.Follow;
+            Debug.Log("Follow");
         }
         else
         {
-            followWaypoints();
+            if (actionStatus == Status.Follow){
+                actionStatus = Status.Search;
+                Debug.Log("Start Search");
+                //StartSearchingTime = Time.time;
+                searchFinished = false;
+            }
+            else
+            {
+                if (actionStatus == Status.Search && searchFinished) {
+                    actionStatus = Status.Patrol;
+                    Debug.Log("Patrolling");
+                }
+            } 
+            
         }
 
-        //Niveau 3
-        //Move(thePlayer.position);
+
+
+        switch(actionStatus)
+        {
+            case Status.Patrol:
+                followWaypoints();
+                break;
+            case Status.Follow:
+                Move(thePlayer.position);
+                break;
+            case Status.Search:
+                //tourne sur lui même
+                search();
+                break;
+        }
+        
     }
 }
